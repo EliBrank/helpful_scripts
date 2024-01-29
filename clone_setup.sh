39428f71101dc716ceb25c258670da8e41a39b28
EliBrank <eliasbrinkman@gmail.com>
2024-01-29 23:51:18 +0000

    Re-edit clone-setup script

--- clone_setup.sh
+++ clone_setup.sh
@@ -4,5 +4,6 @@ read -p "Please paste your GitHub token: " gitToken
 read -p "Please paste in the link to the GitHub repo you wish to clone: " gitRepo
 
 gitRepo=$(echo "$gitRepo" | sed 's|https://||')
+repoName=$(echo ${gitRepo%.git} | awk -F "/" '{print $NF}')
 
-git clone https://$gitToken@$gitRepo
+git clone https://$gitToken@$gitRepo ~/$repoName
