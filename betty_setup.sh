0e8624d24a38ea492d10b059bc365ab69dcb5e35
EliBrank <114535414+EliBrank@users.noreply.github.com>
2024-01-29 16:49:51 -0600

    Update betty_setup.sh

--- betty_setup.sh
+++ betty_setup.sh
@@ -18,15 +18,15 @@ BIN_PATH="/usr/local/bin"
 BETTY_STYLE="betty-style"
 BETTY_DOC="betty-doc"
 
-if [ "$#" = "0" ]; then
+if [ "\$#" = "0" ]; then
     echo "No arguments passed."
     exit 1
 fi
 
-for argument in "$@" ; do
-    echo -e "\n========== $argument =========="
-    ${BIN_PATH}/${BETTY_STYLE} "$argument"
-    ${BIN_PATH}/${BETTY_DOC} "$argument"
+for argument in "\$@" ; do
+    echo -e "\n========== \$argument =========="
+    \${BIN_PATH}/${BETTY_STYLE} "\$argument"
+    \${BIN_PATH}/${BETTY_DOC} "\$argument"
 done
 EOF
 
